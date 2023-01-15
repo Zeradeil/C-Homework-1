73f47a2e3ca888fbed0d715cc76c37f10bbe44cb
Zeradeil <wandark@ya.ru>
2023-01-15 17:09:29 +0700

    add code vs 1

---
+++ task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 ->  max = 7, min = 5
+// a = -9 b = -3 -> max = -3, min = -9
+
+Console.Clear();
+
+System.Console.WriteLine("Введите a");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b){
+    Console.WriteLine($"max = {a} min = {b}");
+}
+if (a == b){
+    Console.WriteLine($"Числа a и b равны {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b} min = {a}");
+}
---
+++ task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисе
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+System.Console.Clear();
+
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max1 = Math.Max(a, b);
+int max = Math.Max(max1, c);
+System.Console.WriteLine($"Максимальное {max}");
